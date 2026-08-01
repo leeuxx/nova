643a80a190aa14fe722631f39553b90d2dd9867e
lijin <4567640597@qq.com>
2026-08-02 00:00:16 +0800

    init

--- nova-core/src/main/java/com/nova/annotation/Nova.java
+++ nova-core/src/main/java/com/nova/annotation/Nova.java
@@ -33,7 +33,7 @@ public @interface Nova {
     RowOperation[] rowOperation() default {};
 
     @Comment("树结构配置")
-    TreeType tree() default @TreeType(value = false);
+    TreeType tree() default @TreeType(value = false, searchField = "");
 
     @Comment("数据钻取")
     Drill[] drills() default {};
