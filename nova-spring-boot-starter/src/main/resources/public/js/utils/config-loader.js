cc53a1d7a16c5f696856f0602d96a6371cbd5871
lijin <4567640597@qq.com>
2026-08-13 15:05:25 +0800

    init

--- nova-spring-boot-starter/src/main/resources/public/js/utils/config-loader.js
+++ nova-spring-boot-starter/src/main/resources/public/js/utils/config-loader.js
@@ -3,9 +3,9 @@
   var DEFAULTS = {
     // 系统名称
     name: 'Nova UI Admin',
-    // 登录页描述
+    // 描述
     desc: '简洁高效的后台管理系统',
-    // 登录页版权信息
+    // 版权信息
     copyrightTxt: '© 2026 Nova UI Admin. All Rights Reserved.',
     // logo 图片
     logo: 'logo.png',
@@ -18,7 +18,7 @@
         // 是否允许修改
         edit: false
     },
-    // 右上角用户下拉自定义项：{ type（fold（下拉菜单内）/ button（铃铛左侧图标按钮，悬浮展示 name））, name, icon, click }
+    // 右上角用户自定义项：{ type（fold（下拉菜单内）/ button（铃铛左侧图标按钮，悬浮展示 name））, name, icon, click }
     tools: []
   }
 
