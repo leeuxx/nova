412f316aa8ff32dc0c70df421a249d53db48c59e
lijin <4567640597@qq.com>
2026-08-13 14:37:44 +0800

    init

--- nova-core/src/main/java/xyz/nova/annotation/sub/nova/field/edit/ShowBy.java
+++ nova-core/src/main/java/xyz/nova/annotation/sub/nova/field/edit/ShowBy.java
@@ -4,7 +4,7 @@ import xyz.nova.annotation.comment.Comment;
 
 public @interface ShowBy {
 
-    @Comment("条件表达式，纯前端实现，使用字段属性名来运算（== != > >= < <= == null != null，支持 && || 和括号分组）")
+    @Comment("条件表达式，纯前端实现，使用字段属性名来运算（== != > >= < <= ==null !=null，支持 && || 括号分组）")
     String value();
 
 }
