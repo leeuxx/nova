805c251290a65526c19644e91b3f9e4d15d3a6e5
lijin <4567640597@qq.com>
2026-08-14 11:55:50 +0800

    init

--- nova-core/src/main/java/xyz/nova/annotation/sub/nova/field/edit/TagFetchHandler.java
+++ nova-core/src/main/java/xyz/nova/annotation/sub/nova/field/edit/TagFetchHandler.java
@@ -1,9 +1,12 @@
 package xyz.nova.annotation.sub.nova.field.edit;
 
+import xyz.nova.annotation.comment.Comment;
+
 import java.util.List;
 
 public interface TagFetchHandler {
 
+    @Comment("获取标签列表")
     List<String> fetchTags(String param);
 
 }
