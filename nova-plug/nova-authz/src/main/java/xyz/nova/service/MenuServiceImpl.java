6ebbecbb0191befe6e2bf160266bd8a73ac9b2c5
lijin <4567640597@qq.com>
2026-08-20 13:44:30 +0800

    init

--- nova-plug/nova-authz/src/main/java/xyz/nova/service/MenuServiceImpl.java
+++ nova-plug/nova-authz/src/main/java/xyz/nova/service/MenuServiceImpl.java
@@ -1,5 +1,6 @@
 package xyz.nova.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.yitter.idgen.YitIdHelper;
@@ -36,19 +37,23 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements Da
 
     @Override
     public void delete(List<MenuNova> menuNova) {
-
+        List<Long> ids = menuNova.stream().map(MenuNova::getId).toList();
+        cascadeDelete(ids);
     }
 
     @Override
     public void update(MenuNova menuNova) {
-        DataProxy.super.update(menuNova);
+        Menu menu = BeanCopyUtils.copy(menuNova, Menu.class);
+        if (menuNova.getMenuNova() != null) {
+            menu.setParentId(menuNova.getMenuNova().getId());
+        }
+        updateById(menu);
     }
 
     @Override
     public MenuNova details(Details details) {
         Menu menu = getById(details.getValue());
-        MenuNova menuNova = new MenuNova();
-        BeanUtils.copyProperties(menu, menuNova);
+        MenuNova menuNova = BeanCopyUtils.copy(menu, MenuNova.class);
         if (menu.getParentId() != null) {
             Menu parentMenu = getById(menu.getParentId());
             menuNova.setMenuNova(new MenuNova()
@@ -70,24 +75,41 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements Da
                 .collect(Collectors.partitioningBy(menu -> menu.getParentId() == null));
         // 将根菜单列表转换为 MenuNova 对象列表
         List<MenuNova> rootList = partitioned.get(true).stream()
-                .map(menu -> {
-                    MenuNova menuNova = new MenuNova();
-                    BeanUtils.copyProperties(menu, menuNova);  // 属性拷贝，忽略不匹配的字段
-                    return menuNova;
-                })
+                .map(menu -> BeanCopyUtils.copy(menu, MenuNova.class))
                 .collect(Collectors.toList());
         // 将子菜单列表转换为 MenuNova 对象列表
         List<MenuNova> childrenList = partitioned.get(false).stream()
-                .map(menu -> {
-                    MenuNova menuNova = new MenuNova();
-                    BeanUtils.copyProperties(menu, menuNova);  // 属性拷贝
-                    menuNova.setMenuNova(new MenuNova().setId(menu.getParentId()));
-                    return menuNova;
-                })
+                .map(menu -> BeanCopyUtils.copy(menu, MenuNova.class)
+                        .setMenuNova(new MenuNova().setId(menu.getParentId()))
+                )
                 .collect(Collectors.toList());
         // 封装返回结果
         return new Tree.Vo<MenuNova>()
                 .setRootList(rootList)
                 .setChildrenList(childrenList);
+    }
+
+    /**
+     * 递归删除菜单
+     * @param ids 菜单id
+     */
+    private void cascadeDelete(List<Long> ids) {
+        if (ids == null || ids.isEmpty()) {
+            return;
+        }
+        // 删除当前菜单
+        removeBatchByIds(ids);
+        // 查询下级菜单
+        List<Menu> menus = list(new LambdaQueryWrapper<Menu>()
+                .select(Menu::getId)
+                .in(Menu::getParentId, ids)
+        );
+        // 递归删除下级菜单
+        if (menus != null && !menus.isEmpty()) {
+            List<Long> childIds = menus.stream()
+                    .map(Menu::getId)
+                    .toList();
+            cascadeDelete(childIds);
+        }
     }
 }
