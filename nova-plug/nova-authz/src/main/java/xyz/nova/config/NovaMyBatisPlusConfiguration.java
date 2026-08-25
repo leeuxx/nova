f6cff2e2c02b4c2f4c714d3b0340bebe574bc5bb
lijin <4567640597@qq.com>
2026-08-25 16:01:47 +0800

    init

--- nova-plug/nova-authz/src/main/java/xyz/nova/config/NovaMyBatisPlusConfiguration.java
+++ nova-plug/nova-authz/src/main/java/xyz/nova/config/NovaMyBatisPlusConfiguration.java
@@ -1,10 +1,31 @@
 package xyz.nova.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import xyz.nova.constant.NovaConst;
 
 @Configuration
 @MapperScan(NovaConst.MAPPER_PACKAGE)
 public class NovaMyBatisPlusConfiguration {
+
+    /**
+     * 分页插件 - 只有当业务方没有自己配置时才生效
+     */
+    @Bean
+    @ConditionalOnMissingBean(MybatisPlusInterceptor.class)
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        // 设置最大单页限制（默认 500 条，-1 表示不限制）
+        paginationInnerInterceptor.setMaxLimit(-1L);
+        // 溢出总页数后是否调整到第一页
+        paginationInnerInterceptor.setOverflow(false);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
+    }
 }
